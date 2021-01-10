68b9cb4bc899040685691468425df0ee25755a66
Neil Slinger <neil.slinger@jpmorgan.com>
2021-01-10 21:37:21 +0000

    Initial commit for base Modular subsite

---
+++ modular/74fd887f.f5ec7638.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{76:function(a){a.exports=JSON.parse('{"allTagsPath":"/modular/blog/tags","slug":"docusaurus","name":"docusaurus","count":3,"permalink":"/modular/blog/tags/docusaurus"}')}}]);
---
+++ modular/d0aff383.3fcd7510.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[19],{87:function(a){a.exports=JSON.parse('{"allTagsPath":"/modular/blog/tags","slug":"hola","name":"hola","count":1,"permalink":"/modular/blog/tags/hola"}')}}]);
